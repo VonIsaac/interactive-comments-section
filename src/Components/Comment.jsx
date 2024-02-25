5df9262a61f736e709eb615502acf8c530694c78
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-02-25 16:32:37 +0800

    adding code and files

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,7 +1,21 @@
 import DATA from '../data.js';
-export default function Comment({comments, reply, onDelete}){
+import ModalDelete from './ModalDelete.jsx';
+import { useRef } from 'react';
+export default function Comment({comments, onDelete}){
+    const openModalDelete = useRef();
+
+    function opendModal(){
+        openModalDelete.current.open()
+    }
 
     return(
+        <>
+
+
+            <ModalDelete ref={openModalDelete} button = "NO CANCEL" onDeletes = { onDelete}  >
+                <h2>Delete comment</h2>
+                <p>Are you sure you want to delete this comment? This will remove the comment and cant be undone</p>
+            </ModalDelete>   
             <ul>     
                 {comments.map((replycomment) => (
                     <div className='border-0 border-slate-950 w-[73%] p-4 mb-4 bg-slate-100 rounded-xl ml-10'>
@@ -11,7 +25,7 @@ export default function Comment({comments, reply, onDelete}){
                                 <img src={DATA[3].img} alt= 'icon' className='w-9 object-contain' />
                                 <h1 className=' font-bold text-lg cursor-pointer'>juliusomo</h1>
                                 <p className=' text-slate-500 font-medium mt-1'>Now</p>
-                                <button onClick={() => onDelete(replycomment.id)} className=' ml-auto font-bold mb-3 cursor-pointer text-rose-700  hover:text-slate-950'>Delete</button>
+                                <button onClick={opendModal} className=' ml-auto font-bold mb-3 cursor-pointer text-rose-700  hover:text-slate-950'>Delete</button>
                             </div>
                             <p className=" font-sans text-slate-600 font-medium ml-5">
                                 <b className=' text-blue-600 cursor-pointer'>{DATA[0].names}</b> {replycomment.comment}
@@ -20,8 +34,9 @@ export default function Comment({comments, reply, onDelete}){
                         </li>
                     </div>
                 ))}
-            </ul>
+            </ul>    
+        </>
     )
 }
 
-                    
+//() => onDelete(replycomment.id)
--- src/Components/ModalDelete.jsx
+++ src/Components/ModalDelete.jsx
@@ -1,26 +1,30 @@
+
 import { useRef, useImperativeHandle,forwardRef  } from "react"
 import { createPortal } from "react-dom"
-const Modal = forwardRef( function Modal({children, button}, ref){
-    const modal = useRef()
+const ModalDelete = forwardRef( function ModalDelete({children, button, onDeletes}, ref){
+    const modalDelete = useRef()
 
     useImperativeHandle(ref,() => {
          return{
             open(){
-                modal.current.showModal()
+                modalDelete.current.showModal()
             } 
          }
     })
-     
+
+    
     return createPortal(
         
-            <dialog ref={modal} className=" backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+            <dialog ref={modalDelete} className=" backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
                 {children}
+                <button onClick={ onDeletes}>YES</button>
                 <form method="dialog">
+              
                     <button className=" px-4 py-2 text-xs md:text-base rounded-md bg-stone-800 text-stone-400 hover:bg-stone-600 hover:text-stone-100 ">{button}</button>
                 </form>
             </dialog>,
-            document.getElementById('modal-root')
+            document.getElementById('modal-delete')
     )
 })
 
-export default  Modal
+export default ModalDelete
