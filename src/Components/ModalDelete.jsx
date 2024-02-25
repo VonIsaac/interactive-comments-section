56868ced28127d992ddd7743c7f33094e1e52f67
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-02-25 18:03:17 +0800

    adiing and styling

--- src/Components/ModalDelete.jsx
+++ src/Components/ModalDelete.jsx
@@ -16,12 +16,12 @@ const ModalDelete = forwardRef( function ModalDelete({children, button, onDelete
     
     return createPortal(
         
-            <dialog ref={modalDelete} className=" backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+            <dialog ref={modalDelete} className=" backdrop:bg-stone-900/90 p-4 rounded-md shadow-md w-[25%]">
                 {children}
-                <button onClick={ onDeletes}>YES</button>
-                <form method="dialog">
-              
-                    <button className=" px-4 py-2 text-xs md:text-base rounded-md bg-stone-800 text-stone-400 hover:bg-stone-600 hover:text-stone-100 ">{button}</button>
+                
+                <form method="dialog" className= "gap-3">
+                    <button className=" px-2 py-1 text-xs md:text-base rounded-md bg-stone-800 text-stone-400 hover:bg-stone-600 hover:text-stone-100 w-[27%]">{button}</button>
+                    <button onClick={ onDeletes} className=" ml-3 bg-stone-800 text-stone-400 px-2 py-1 rounded hover:bg-stone-600 hover:text-stone-100">YES, DELETE</button>
                 </form>
             </dialog>,
             document.getElementById('modal-delete')
